a85284ffa910526d3264873a6b503ab842e04bb1
Artwalk <frank.holy.art@gmail.com>
2014-02-23 11:36:28 +0800

    add today news

--- main.go
+++ main.go
@@ -34,79 +34,80 @@ func zhihuDailyJson(str string) UsedData {
 	return UsedData{Date: date, News: news}
 }
 
-func renderPages(tatal int) map[int]FinalData {
+func renderPages(days int) map[int]FinalData {
 	memoreyCache := QueryData()
 
-	page := make(map[int]FinalData)
-
+	pages := make(map[int]FinalData)
 	var pagemark []int
-	for i := len(memoreyCache) / tatal; i > 0; i -= 1 {
+	date := time.Now()
+
+	for i := 1; i <= len(memoreyCache)/days; i += 1 {
 		pagemark = append(pagemark, i)
 	}
 
-	date := time.Now()
+	for i := 1; i <= len(memoreyCache)/days; i += 1 {
 
-	i := len(memoreyCache) / tatal
-	for ; i > 0; i -= 1 {
 		var finaldata FinalData
 		var useddata []UsedData
-		for j := 0; j < tatal; j++ {
-			temp, _ := strconv.Atoi(date.Format("20060102"))
 
-			data, ok := memoreyCache[temp]
-			if ok {
-				useddata = append(useddata, zhihuDailyJson(data))
-			} else {
-				url := "http://news.at.zhihu.com/api/1.2/news/before/" + date.Format("20060102")
+		if i == 1 {
+			useddata = append(useddata, zhihuDailyJson(todayData()))
+		}
 
-				data = getData(url)
-				dateInt, _ := strconv.Atoi(date.Format("20060102"))
-				writeToDB(dateInt, data)
-				useddata = append(useddata, zhihuDailyJson(data))
+		for j := 0; j < days; j++ {
+			key := date.Format("20060102")
+
+			data, ok := memoreyCache[atoi(key)]
+			if !ok {
+				data = getBeforeData(key)
 			}
+
+			useddata = append(useddata, zhihuDailyJson(data))
 			date = date.AddDate(0, 0, -1)
 		}
 		finaldata.Useddata = useddata
 		finaldata.Pagemark = pagemark
-		page[i] = finaldata
+		pages[i] = finaldata
 	}
 
-	return page
+	return pages
 }
 
-func initDB() {
-	InitDB()
-	GetBeforeData()
+func atoi(s string) int {
+	dateInt, _ := strconv.Atoi(s)
+	return dateInt
 }
 
 func main() {
-	// initDB()
-	pages := renderPages(3)
+
+	pages := renderPages(4)
 
 	m := martini.Classic()
 	m.Use(martini.Static("static"))
 	m.Use(render.Renderer())
 
-	http.ListenAndServe("0.0.0.0:8080", m)
+	lastUpdate := time.Now()
+
+	// http.ListenAndServe("0.0.0.0:8000", m)
 
 	// todayData := zhihuDailyJson(mydatabase.TodayData())
 	// finalData = []interface{}{todayData}
 
 	m.Get("/", func(r render.Render) {
-		r.HTML(200, "content", []interface{}{pages[len(pages)]})
+		if time.Since(lastUpdate) > time.Hour {
+			pages = renderPages(5)
+		}
+		r.HTML(200, "content", []interface{}{pages[1]})
 	})
 
 	m.Get("/date/:id", func(params martini.Params, r render.Render) {
 
-		id, _ := strconv.Atoi(params["id"])
-		fmt.Println("id", id)
+		id := atoi(params["id"])
+
 		r.HTML(200, "content", []interface{}{pages[id]})
 	})
 
-	// each title
 	m.Get("/url/**", func(params martini.Params, r render.Render) {
-
-		// fmt.Println(params["_1"])
 
 		r.HTML(200, "share_image", params["_1"])
 	})
@@ -146,28 +147,29 @@ func QueryData() map[int]string {
 		memoryCache[date] = data
 	}
 
-	// fmt.Println(memoryCache[20131212])
 	return memoryCache
 }
 
-func GetBeforeData() {
-
-	// string -> time
+func getAllData() {
 	date, _ := time.Parse("20060102", "20140209")
 	firstDate, _ := time.Parse("20060102", "20130520")
 
 	for ; date.After(firstDate); date = date.AddDate(0, 0, -1) {
-
-		url := "http://news.at.zhihu.com/api/1.2/news/before/" + date.Format("20060102")
-
-		data := getData(url)
-		dateInt, _ := strconv.Atoi(date.Format("20060102"))
-		writeToDB(dateInt, data)
+		getBeforeData(date.Format("20060102"))
 	}
+
 }
 
-func TodayData() string {
-	// today := time.Now().Format("20060102")
+func getBeforeData(date string) string {
+	url := "http://news.at.zhihu.com/api/1.2/news/before/" + date
+	data := getData(url)
+
+	writeToDB(atoi(date), data)
+
+	return data
+}
+
+func todayData() string {
 	url := "http://news.at.zhihu.com/api/1.2/news/latest"
 
 	return getData(url)
