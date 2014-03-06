490bfc59e50afd7d40cea52cc057ce9790010cb1
Artwalk <frank.holy.art@gmail.com>
2014-03-06 11:49:48 +0800

    add run.sh

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo 'updating git'
+rm main.db
+git pull
+git checkout main.db
+go build main.go
+
+echo 'Killing old main'
+for a in `ps -a | grep 'main'`
+do
+	if [[ "$a" != *[^0-9]* ]]&&[[ "$a" != 0* ]]
+		then `kill -9 $a`
+	fi
+	break;
+done
+
+echo 'Starting run main...'
+./main &
+disown %1
+echo 'Done'
